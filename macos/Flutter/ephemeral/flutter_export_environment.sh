9a07346572f73b9ecfd882124ca03bbb9f1e571c
givazahra <zahratussitaanggifa@gmail.com>
2025-09-03 13:55:40 +0700

    tampilan seluruh aplikasi app iuran dummy

---
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=D:\DOKUMEN_FILE\Android\flutter"
+export "FLUTTER_APPLICATION_PATH=D:\ドキュメント\TUGAS GURU SMK 1 CIANJUR\PROGRES PKL\iuran-app\iuran"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=0.1.0"
+export "FLUTTER_BUILD_NUMBER=0.1.0"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
